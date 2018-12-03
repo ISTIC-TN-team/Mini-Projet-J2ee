565a2649cead4e7375e2e901b997a82d8d4cd39e
zone52 <zone52@zone52-PC>
2018-12-03 19:56:59 +0100

    comment / constructors

--- src/java/database/ArticleDao.java
+++ src/java/database/ArticleDao.java
@@ -1,7 +1,10 @@
 package database;
 import beans.Article;
 import java.sql.*;  
-
+/**
+ **Author Skanderbelgaied / Heni abdmouleh  / Marwen Bougossa 
+ ** Date :2018 - 2019
+ **/
 public class ArticleDao {
     
  public Article getClient(String libelle){
