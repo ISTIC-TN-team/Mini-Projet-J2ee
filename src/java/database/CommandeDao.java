a767017d15501908b4403901f2faebad385d4732
zone52 <zone52@zone52-PC>
2018-12-03 19:38:58 +0100

    modifie commandeDao

--- src/java/database/CommandeDao.java
+++ src/java/database/CommandeDao.java
@@ -3,9 +3,32 @@ import beans.Commande;
 import java.sql.*;
 
 public class CommandeDao {
-    
+    Connection con = DBConnexion.getInstance();
+     public boolean addCommande(Commande a)
+    {
+        try
+        {
+            String req = "INSERT INTO `w-shopdb`.`commande` (`login`, `prixtotale`) "
+                    + "VALUES ('"+a.getLogin()+"', '"+a.getPrixTotale()+"');";
+
+            Statement st = con.createStatement();
+
+            int rs = st.executeUpdate(req);
+
+            if (rs > 0)
+            {
+                return true;
+            }
+
+        } catch (Exception e1)
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        return false;
+    }
 	public Commande getCommande(int id){
-    Connection con=DBConnexion.getInstance();
     Commande c=new Commande();
     PreparedStatement statement;
     String sql="SELECT *  FROM `commande` WHERE `idcommande` =?";
