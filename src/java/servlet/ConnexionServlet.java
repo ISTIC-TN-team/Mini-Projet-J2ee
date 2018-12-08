fe3aee5fdaf5db7d6c93cfda04c6fd8d54fd8e33
marwen bougossa <marwenbg123@gmail.com>
2018-12-08 16:51:27 +0100

    servlet

---
+++ src/java/servlet/ConnexionServlet.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import beans.Client;
+import database.ClientDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author clickinformatique
+ */
+public class ConnexionServlet extends HttpServlet {
+
+    private Client NULL;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){}
+    @Override
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+     String login =  request.getParameter("login");
+     String password = request.getParameter("password");
+     ClientDao a = new ClientDao();
+     Client s = a.getClient(login);
+     String er = "username et mot de passe incorrecte";
+     if(s == NULL ){
+     response.sendRedirect("login.jsp?msg="+er);
+     }
+     else{
+     request.getSession().setAttribute("client",s);
+     response.sendRedirect("index.jsp");
+     }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
